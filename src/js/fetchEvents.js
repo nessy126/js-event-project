18a594397e4119dd3718d4808bf452d6b74aed7d
OKirsenko <senka3434@gmail.com>
2022-01-21 12:39:08 +0200

    Update fetchEvents.js

--- src/js/fetchEvents.js
+++ src/js/fetchEvents.js
@@ -1,1 +1,22 @@
 const axios = require('axios');
+
+const baseUrl =
+  'https://app.ticketmaster.com/discovery/v2/events.json?apikey=peAFicxdgROimLgt9qctrhItXcX4ekFy';
+let pageCount = 1;
+let keyword = '';
+let countryCode = '';
+
+async function fetchEvents(keyword, countryCode) {
+  try {
+    const result = await axios.get(
+      `${baseUrl}&page=${pageCount}&keyword=${keyword}&countryCode=${countryCode}`,
+    );
+
+    const events = result.data._embedded.events;
+
+    return events;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export { pageCount, keyword, countryCode, fetchEvents };
