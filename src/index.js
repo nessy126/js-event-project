02c3c8047c02b3259c49fd87a15a4757dab62d94
Smilyk Kostiantyn <bmd.Warher@gmail.com>
2022-01-21 16:48:01 +0200

    Scroll

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import "./sass/main.scss";
+import './sass/main.scss';
+import renderMarkupMain from './js/renderMarkupMain';
+import modalTogle from './js/modal-team';
+import modalTogle from './js/scroll';
+
+modalTogle();
+onScroll();
+onToUpBtn();
