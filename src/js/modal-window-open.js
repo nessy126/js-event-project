2f74eae7c771ba2c670db28ee385f477e1de4c5a
Roman Havryliuk <gavru4r@gmail.com>
2022-01-22 11:25:17 +0200

    Update modal-window-open.js

--- src/js/modal-window-open.js
+++ src/js/modal-window-open.js
@@ -3,12 +3,19 @@ export default function onToggleModal() {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', onBackdrop);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+  }
+  function onBackdrop(e) {
+    if (e.target === e.currentTarget) {
+      toggleModal();
+    }
   }
 }
