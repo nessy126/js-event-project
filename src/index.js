064b2866cc0060884dd579fa5ed35d19a643f685
Roman Havryliuk <gavru4r@gmail.com>
2022-01-24 13:29:09 +0200

    fix modal window

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/main.scss';
 import renderMarkupMain from './js/renderMarkupMain';
 import modalTogle from './js/modal-team';
-import onToggleModal from './js/modal-window-open';
+import './js/modal-window-open';
 import { params, fetchEvents } from './js/fetchEvents';
 import './js/scroll';
 import './js/fetchEvents';
@@ -11,5 +11,3 @@ import './js/onSubmit';
 let { countryCode, keyword, pageCount } = params;
 
 fetchEvents(keyword, countryCode).then(renderMarkupMain);
-// modalTogle();
-// onToggleModal();
--- src/js/renderMarkupMain.js
+++ src/js/renderMarkupMain.js
@@ -3,7 +3,7 @@ export default function renderMarkupMain(array) {
   const markupCard = array
     .map(
       item =>
-        `<li class="gallery__item"><div class="gallery__block"><div class="gallery__decoration"></div><img data-modal-open class="gallery__img" src="${item.images[0].url}" alt="${item.name}"><p class="gallery__title">${item.name}</p><p class='gallery__date'>${item.dates.start.localDate}</p><p class="gallery__place">${item._embedded.venues[0].name}</p></div></li>`,
+        `<li data-modal-open class="gallery__item"><div class="gallery__block"><div class="gallery__decoration"></div><img class="gallery__img"  src="${item.images[0].url}" alt="${item.name}"><p class="gallery__title">${item.name}</p><p class='gallery__date'>${item.dates.start.localDate}</p><p class="gallery__place">${item._embedded.venues[0].name}</p></div></li>`,
     )
     .join('');
   return (galleryEl.innerHTML = markupCard);
