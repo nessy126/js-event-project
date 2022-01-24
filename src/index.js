ee53c31835e5d739d129126ed56ee02c491b57a8
Smilyk Kostiantyn <bmd.Warher@gmail.com>
2022-01-24 16:18:08 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import './sass/main.scss';
 import renderMarkupMain from './js/renderMarkupMain';
-import modalTogle from './js/modal-team';
-import onToggleModal from './js/modal-window-open';
+import  './js/modal-team';
+import './js/modal-window-open';
 import { params, fetchEvents } from './js/fetchEvents';
 import './js/scroll';
 import './js/fetchEvents';
 import './js/markupSelect';
 import './js/onSubmit';
+import './js/styleSelect';
 
 let { countryCode, keyword, pageCount } = params;
+fetchEvents(keyword, countryCode, pageCount).then(renderMarkupMain);
 
-fetchEvents(keyword, countryCode).then(renderMarkupMain);
-// modalTogle();
-// onToggleModal();
