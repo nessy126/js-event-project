9215bad8935ea7961d8b1266cb222cd5262143ca
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-01-26 11:36:18 +0200

    Update function generatePaginationUsingRightData

--- src/js/generatePaginationUsingRightData.js
+++ src/js/generatePaginationUsingRightData.js
@@ -3,7 +3,7 @@ import { params, fetchEvents, requestToAPI } from './fetchEvents';
 import paginationMarkup from './pagination';
 
 let { countryCode, keyword, pageCount } = params;
-fetchEvents(keyword, countryCode, pageCount).then(renderMarkupMain);
+// fetchEvents(keyword, countryCode, pageCount).then(renderMarkupMain);
 
 const paginationId = document.querySelector('.pagination');
 fetchEvents(keyword, countryCode, pageCount).then(
